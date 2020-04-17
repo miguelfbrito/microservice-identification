bb6d1e3bb0e43556f5d814f5b5313c37d3630aa7
Miguel Brito <miguelfbrito11@gmail.com>
2020-04-17 16:55:31 +0100

    Add test cases for CHD (project level). Add missing StringUtils for CHD

--- symbolsolver/src/main/java/utils/StringUtils.java
+++ symbolsolver/src/main/java/utils/StringUtils.java
@@ -1,27 +1,40 @@
 package utils;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtils {
 
-    public static Map<String, Integer> readClustersFromString(String string){
+    public static Map<String, Integer> readClustersFromString(String string) {
         String pattern = "'([\\w\\.]*)'";
         String[] stringSplit = string.split("\\]");
 
         int group = 0;
         Map<String, Integer> clusters = new HashMap<>();
-        Pattern  p = Pattern.compile(pattern);
+        Pattern p = Pattern.compile(pattern);
 
-        for(String s : stringSplit){
+        for (String s : stringSplit) {
             Matcher m = p.matcher(s);
-            while(m.find()){
+            while (m.find()) {
                 clusters.put(m.group(1), group);
             }
             group++;
         }
         return clusters;
+    }
+
+    public static Set<String> extractCamelCase(String string) {
+        return new HashSet<>(Arrays.asList(string.split("(?<=[a-z])(?=[A-Z])")));
+    }
+
+    public static Set<String> extractCamelCaseLower(String string) {
+        Set<String> extracted = new HashSet<>();
+        for (String s : string.split("(?<=[a-z])(?=[A-Z])")) {
+            extracted.add(s.toLowerCase());
+        }
+        return extracted;
     }
 }
