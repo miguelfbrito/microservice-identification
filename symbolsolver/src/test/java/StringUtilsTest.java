1044caebba03d598117a3b7acbce330f48d7321a
Miguel Brito <miguelfbrito11@gmail.com>
2020-04-21 11:29:45 +0100

    Reorganize and update tests

--- symbolsolver/src/test/java/StringUtilsTest.java
+++ symbolsolver/src/test/java/StringUtilsTest.java
@@ -11,43 +11,43 @@ import java.util.List;
 public class StringUtilsTest {
 
     @Test
-    public void extractFromParameterNormalVariable() {
+    public void shouldExtractSingleVariable() {
         List<String> extracted = StringUtils.extractVariableType("Visit");
         assertEquals(Collections.singletonList("Visit"), extracted);
     }
 
     @Test
-    public void extractGenerics() {
+    public void shouldExtractGeneric() {
         List<String> extracted = StringUtils.extractVariableType("Collection<T>");
         assertEquals(Collections.singletonList("T"), extracted);
     }
 
     @Test
-    public void extractFromCollectionSingleElement() {
+    public void shouldExtractSingleVariableFromCollection() {
         List<String> extracted = StringUtils.extractVariableType("Collection<Visit>");
         assertEquals(Collections.singletonList("Visit"), extracted);
     }
 
     @Test
-    public void extractArrayTypeFromString() {
+    public void shouldExtractArrayType() {
         List<String> extracted = StringUtils.extractVariableType("Integer[][]");
         assertEquals(Collections.singletonList("Integer"), extracted);
     }
 
     @Test
-    public void extractFromCollectionMultipleElements() {
-        List<String> extracted = StringUtils.extractVariableType("Collection<String, Visit>");
+    public void shouldExtractMultipleVariablesFromCollection() {
+        List<String> extracted = StringUtils.extractVariableType("Map<String, Visit>");
         assertEquals(Arrays.asList("String", "Visit"), extracted);
     }
 
     @Test
-    public void extractFromCollectionNestedElements() {
+    public void shouldExtractMultipleVariablesFromNestedCollections() {
         List<String> extracted = StringUtils.extractVariableType("Map<String, Collection<Visit>>");
         assertEquals(Arrays.asList("String", "Visit"), extracted);
     }
 
     @Test
-    public void extractFromCollectionMultipleNestedElements() {
+    public void shouldExtractMultipleVariablesFromMultipleLevelsOfNestedCollections() {
         List<String> extracted = StringUtils.extractVariableType("Map<String, Map<ClassA, Map<ClassB, Collection<Collection<Visit>>");
         assertEquals(Arrays.asList("String", "ClassA", "ClassB", "Visit"), extracted);
     }
