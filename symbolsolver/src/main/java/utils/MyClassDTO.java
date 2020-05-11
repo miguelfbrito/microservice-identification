e996ead19d94d05534db282be66680e73baf2e7b
Miguel Brito <miguelfbrito11@gmail.com>
2020-05-11 17:04:28 +0100

    Export the variable dependencies since the javalang has no symbolsolver capabilities

--- symbolsolver/src/main/java/utils/MyClassDTO.java
+++ symbolsolver/src/main/java/utils/MyClassDTO.java
@@ -1,5 +1,6 @@
 package utils;
 
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.MarkerAnnotationExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.nodeTypes.NodeWithName;
@@ -21,6 +22,8 @@ public class MyClassDTO {
     @Expose
     private List<String> variables;
     @Expose
+    private List<String> variablesDependencies;
+    @Expose
     private Map<String, MyMethod> methods; // method name, method reference
     @Expose
     private Map<String, String> methodInvocations;
@@ -33,11 +36,13 @@ public class MyClassDTO {
         this.qualifiedName = myClass.getQualifiedName();
         this.annotations = myClass.getAnnotations().stream().map(NodeWithName::getNameAsString).collect(Collectors.toList());
         this.variables = myClass.getVariables().stream().map(v -> v.getType().asString() + " " + v.getName().toString()).collect(Collectors.toList());
+        this.variablesDependencies = handleVariablesDependencies(myClass.getVariables());
         this.methods = myClass.getMethods();
         this.methodInvocations = handleMethodInvocations(myClass.getMethodInvocations());
         this.implementedTypes = new ArrayList<>(myClass.getImplementedTypes());
         this.extendedTypes = new ArrayList<>(myClass.getExtendedTypes());
     }
+
 
     public Map<String, String> handleMethodInvocations(List<MethodCallExpr> methodInvocations) {
         Map<String, String> processedMethods = new HashMap<>();
@@ -52,10 +57,27 @@ public class MyClassDTO {
                 } catch (UnsupportedOperationException e) {
                     //e.printStackTrace();
                 } catch (RuntimeException e) {
-
+                    // TODO : reevaluate
                 }
             });
         }
         return processedMethods;
+    }
+
+    public List<String> handleVariablesDependencies(List<VariableDeclarator> variableDeclarators) {
+        List<String> varDependencies = new ArrayList<>();
+        for (VariableDeclarator variableDeclarator : variableDeclarators) {
+            try {
+                ResolvedType resolve = variableDeclarator.getType().resolve();
+                resolve.asReferenceType().getQualifiedName();
+                String typeClassName = variableDeclarator.getType().resolve().asReferenceType().getQualifiedName();
+                varDependencies.add(typeClassName);
+            } catch (UnsolvedSymbolException e) {
+                // When it tries to resolve a class not explicitly present in the project. We don't care about those.
+            } catch (UnsupportedOperationException e) {
+                //e.printStackTrace();
+            }
+        }
+        return varDependencies;
     }
 }
