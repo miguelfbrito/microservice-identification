58ff21b0953c1c30753fffc62b2a8f6385720a88
Miguel Brito <miguelfbrito11@gmail.com>
2020-05-28 14:14:38 +0100

    Add CHM and CHD metrics implemented on FoSCI

---
+++ app/metrics/tosc-interf-dom-cohesion.py
@@ -0,0 +1,247 @@
+# Implementation of metrics proposed by FoSCI
+# You can find the original at https://github.com/jinwuxia/RS17_project_program
+
+import sys
+import csv
+import re
+
+global g_clusterID2Interf2APIDict  # [clusterID][interface] = list[api id ...]
+global g_apiDict  # [api id] = api object
+global g_ignore_items
+#api is operation
+
+
+class APIObject:
+    def __init__(self, clusterID, interface, apiName, itemSet):
+        self.clusterID = clusterID
+        self.interface = interface
+        self.apiName = apiName
+        self.itemSet = itemSet
+
+
+def Trans2Set(strstr):
+    if strstr == '':
+        resList = ['void']
+    else:
+        resList = strstr.split(',')
+    return set(resList)
+
+
+def GetInterf(api):
+    # interface name
+    apiList = api.split('.')
+    del apiList[len(apiList) - 1]
+    interface = '.'.join(apiList)
+    return interface
+
+
+def IsIgnored(item):
+    global g_ignore_items
+    if item in g_ignore_items:
+        return True
+    else:
+        return False
+
+# for each ele, split by hump
+
+
+def SplitHump(oneList):
+    resList = list()
+    for name in oneList:
+        upperIndexList = list()
+        upperIndexList.append(0)  # first index
+        for index in range(0, len(name)):
+            if name[index].isupper():
+                upperIndexList.append(index)
+        upperIndexList.append(index + 1)  # last index + 1
+
+        for i in range(0, len(upperIndexList) - 1):
+            index_s = upperIndexList[i]
+            index_e = upperIndexList[i + 1]
+            strstr = name[index_s: index_e]
+            resList.append(strstr)
+    return resList
+
+# split(., tuofeng) each name to items, and ignore the non-domain item
+
+
+def GetItems(nameSet):
+    itemList = list()
+    for name in nameSet:
+        # split, for xwiki, the filter words shoud delete the first three pacakageprefix
+        tmp = name.split('.')
+        if len(tmp) > 3:
+            name = tmp[len(tmp) - 3] + "." + \
+                tmp[len(tmp) - 2] + "." + tmp[len(tmp) - 1]
+        ############
+        tmpList = re.split(r'[._]', name)
+        tmpList = SplitHump(tmpList)
+        tmpList = [each.lower() for each in tmpList]
+        itemList.extend(tmpList)
+    newItemList = list()
+    for item in itemList:
+        if IsIgnored(item) == False and item != '':
+            newItemList.append(item)
+    #print ('before=', set(nameSet))
+    #print ('after =', set(newItemList))
+    return set(newItemList)
+
+
+def ReadAPIFile(fileName):
+    apiID = 0
+    clusterID2Interf2ApiDict = dict()
+    apiDict = dict()
+
+    with open(fileName, 'r', newline="") as fp:
+        reader = csv.reader(fp)
+        for each in reader:
+            [clusterID, interfName, apiName, parameterstr, returnstr] = each
+            if each[0] == 'clusterID':
+                continue
+            clusterID = int(clusterID)
+            parameterSet = Trans2Set(parameterstr)
+            returnSet = Trans2Set(returnstr)
+            interface = GetInterf(apiName)
+            if clusterID not in clusterID2Interf2ApiDict:
+                clusterID2Interf2ApiDict[clusterID] = dict()
+            if interface not in clusterID2Interf2ApiDict[clusterID]:
+                clusterID2Interf2ApiDict[clusterID][interface] = list()
+            nameSet = parameterSet | returnSet
+            nameSet.add(apiName)
+            itemSet = GetItems(nameSet)
+            oneObejct = APIObject(clusterID, interface, apiName, itemSet)
+            # print nameSet
+            # print oneObejct.itemSet
+            apiDict[apiID] = oneObejct
+            clusterID2Interf2ApiDict[clusterID][interface].append(apiID)
+            apiID += 1
+    # print clusterID2Interf2ApiDict
+    # print apiDict
+    return clusterID2Interf2ApiDict, apiDict
+
+# domain-level edge weight
+
+
+def GetEdge(apiID1, apiID2):
+    global g_apiDict
+    # print g_apiDict
+    itemSet1 = g_apiDict[apiID1].itemSet
+    itemSet2 = g_apiDict[apiID2].itemSet
+    # print g_apiDict[apiID1].interface, g_apiDict[apiID1].apiName, itemSet1
+    # print g_apiDict[apiID2].interface, g_apiDict[apiID2].apiName, itemSet2
+
+    if len(itemSet1) == 0 and len(itemSet2) == 0:  # not have domain items, then return 1
+        edge_wei = 1
+        edge_unwei = 1
+
+    interSet = itemSet1 & itemSet2
+    unionSet = itemSet1 | itemSet2
+    #print('itemSet1', itemSet1)
+    #print('itemSet2', itemSet2)
+    #print('unionset', unionSet)
+    if len(unionSet) == 0:
+        return -1, -1
+    edge_wei = len(interSet) / float(len(unionSet))
+    if len(interSet) != 0:
+        edge_unwei = 1.0
+    else:
+        edge_unwei = 0.0
+    return edge_wei, edge_unwei
+
+# get api list for a cluster
+# g_clusterID2Interf2APIDict[clusterID][interface] = [api id ....]
+
+
+def getAllAPIForCluster(clusterID):
+    apiIDList = list()
+    global g_clusterID2Interf2APIDict
+    for interface in g_clusterID2Interf2APIDict[clusterID]:
+        apis = g_clusterID2Interf2APIDict[clusterID][interface]
+        apiIDList.extend(apis)
+    return apiIDList
+
+
+# compute the interface's dom_cohesion
+def Metric_dom_cohesion(clusterID):
+    global g_clusterID2Interf2APIDict
+    apiIDList = getAllAPIForCluster(clusterID)
+    if len(apiIDList) == 1:
+        dom_cohesion_wei = 1.0
+    else:
+        from itertools import combinations
+        apiIDPairList = list(combinations(apiIDList, 2))
+        sim_wei_list = list()
+        for apiIDpair in apiIDPairList:
+            [edge_wei, edge_unwei] = GetEdge(apiIDpair[0], apiIDpair[1])
+            if edge_wei != -1:
+                sim_wei_list.append(edge_wei)
+        dom_cohesion_wei = sum(sim_wei_list) / float(len(sim_wei_list))
+    return dom_cohesion_wei
+
+
+if __name__ == '__main__':
+    apiFileName = sys.argv[1]
+    global g_ignore_items
+    global g_clusterID2Interf2APIDict
+    global g_apiDict
+    g_clusterID2Interf2APIDict = dict()
+    g_apiDict = dict()
+    g_ignore_items = ['jpetstore', 'jforum', 'xwiki', 'roller', 'agilefant', 'blog', 'raysmond',
+                      'b3log', 'solo', 'fi', 'hut', 'soberit', 'agilefant', 'servlets', 'javax',
+                      'java', 'net', 'org', 'util', 'lang', 'apache', 'roller', 'weblogger', 'int',
+                      'math', 'string', 'int', 'void', 'date', 'object', 'list',
+                      'get', 'set', 'decimal', 'boolean', 'action', 'service', 'bean',
+                      'service', 'repository', 'controller', 'data', 'dto', 'util', 'id', 'processor',
+                      'solo', 'service', 'repository', 'process', 'controller', 'data', 'date', 'dto',
+                      'util', 'id', 'processor', 'impl', 'cache', 'mgmt', 'query', 'console',
+                      'service', 'hsqldb', 'type', 'dao', 'acces', 'default', 'generic', 'common', 'action',
+                      'repository', 'process', 'control', 'controller', 'data', 'date', 'dto', 'util', 'id',
+                      'processor', 'impl', 'cache', 'mgmt', 'query', 'console', 'comparator',
+                      'exception', 'provider', 'impl', 'bean', 'edit', 'action', 'interceptor', 'factory',
+                      'util', 'data', 'servlet', 'view', 'base', 'management', 'request', 'cache', 'manage',
+                      'manager', 'manag', 'pager', 'pag', 'model', 'service', 'wrapper', 'wrapp', 'weblog',
+                      'comparator', 'accessor', 'task', 'jpa', 'abstract', 'action', 'container', 'interceptor', 'business',
+                      'impl', 'history', 'load', 'filter', 'hierarchy', 'dao', 'hibernate', 'entry', 'generator',
+                      'agilefant', 'to', 'type', 'data', 'node', 'metric', 'handle', 'manager', 'manage',
+                      'default', 'service', 'config', 'filter', 'filt', 'listen', 'render', 'abstract', 'typ', 'string',
+                      'request',  'resource', 'response', 'object', 'factory', 'access', 'model', 'action', 'abstract',
+                      'customiz', 'generator', 'load',  'build', 'listen', 'descriptor', 'script', 'repository', 'action',
+                      'cache', 'type',  'resolve', 'convert', 'and', 'provid', 'of', 'in', 'list', 'from', 'impl', 'check',
+                      'serializer', 'serialize', 'xwiki', 'wiki', 'context', 'reference', 'translation', 'configuration',
+                      'annotation', 'bridge', 'long']
+
+    [g_clusterID2Interf2APIDict, g_apiDict] = ReadAPIFile(
+        apiFileName)
+
+    if len(g_clusterID2Interf2APIDict) == 0:
+        tmp = ['avg_dom_cohesion', str(1.0), 'interface_number', str(
+            0), 'clusterHasinterface', '0']
+        print(','.join(tmp))
+    else:
+        dom_cohesion_wei_list = list()
+        for clusterID in g_clusterID2Interf2APIDict:
+            dom_cohesion_wei = Metric_dom_cohesion(clusterID)
+            dom_cohesion_wei_list.append(dom_cohesion_wei)
+            #print('cluster,' + str(clusterID) + ',' + str(dom_cohesion_wei))
+        avg_dom_cohesion_wei = sum(
+            dom_cohesion_wei_list) / float(len(dom_cohesion_wei_list))
+
+        interface_number = 0
+        interface_number_list = list()
+        for clusterID in g_clusterID2Interf2APIDict:
+            interface_number += len(g_clusterID2Interf2APIDict[clusterID])
+            interface_number_list.append(
+                len(g_clusterID2Interf2APIDict[clusterID]))
+        tmp = ['avg_dom_cohesion', str(avg_dom_cohesion_wei), 'interface_number', str(
+            interface_number), 'clusterHasinf', str(len(g_clusterID2Interf2APIDict))]
+        print(','.join(tmp))
+
+        '''
+        #print apidetail, using do and m_cohesion_wei_list interface_number_list
+        print('\ninterface dom cohesion detail:')
+        for index in range(0, len(dom_cohesion_wei_list)):
+            print(dom_cohesion_wei_list[index])
+        print('\ninterface number detail:')
+        for index in range(0, len(dom_cohesion_wei_list)):
+            print(interface_number_list[index])
+        '''
---
+++ app/metrics/tosc-interf-msg-cohesion.py
@@ -0,0 +1,176 @@
+# Implementation of metrics proposed by FoSCI
+# You can find the original at https://github.com/jinwuxia/RS17_project_program
+
+
+import sys
+import csv
+
+global g_clusterID2Interf2APIDict  # [clusterID][interface] = list[api id ...]
+global g_apiDict  # [api id] = api object
+
+#api is operation
+
+
+class APIObject:
+    def __init__(self, clusterID, interface, apiName, parameterSet, returnSet):
+        self.clusterID = clusterID
+        self.interface = interface
+        self.apiName = apiName
+        self.parameterSet = parameterSet
+        self.returnSet = returnSet
+
+
+def Trans2Set(strstr):
+    if strstr == '':
+        resList = ['void']
+    else:
+        resList = strstr.split(',')
+    return set(resList)
+
+
+def GetInterf(api):
+    # interface name
+    apiList = api.split('.')
+    del apiList[len(apiList) - 1]
+    interface = '.'.join(apiList)
+    return interface
+
+
+def ReadAPIFile(fileName):
+    apiID = 0
+    clusterID2Interf2ApiDict = dict()
+    apiDict = dict()
+
+    with open(fileName, 'r', newline="") as fp:
+        reader = csv.reader(fp)
+        for each in reader:
+            [clusterID, interfName, apiName, parameterstr, returnstr] = each
+            if each[0] == 'clusterID':
+                continue
+            clusterID = int(clusterID)
+            parameterSet = Trans2Set(parameterstr)
+            returnSet = Trans2Set(returnstr)
+            interface = GetInterf(apiName)
+            if clusterID not in clusterID2Interf2ApiDict:
+                clusterID2Interf2ApiDict[clusterID] = dict()
+            if interface not in clusterID2Interf2ApiDict[clusterID]:
+                clusterID2Interf2ApiDict[clusterID][interface] = list()
+            oneObejct = APIObject(clusterID, interface,
+                                  apiName, parameterSet, returnSet)
+            apiDict[apiID] = oneObejct
+            clusterID2Interf2ApiDict[clusterID][interface].append(apiID)
+            apiID += 1
+    # print clusterID2Interf2ApiDict
+    # print apiDict
+    return clusterID2Interf2ApiDict, apiDict
+
+
+def GetIntersect(apiID1, apiID2):
+    global g_apiDict
+    para_interset = g_apiDict[apiID1].parameterSet & g_apiDict[apiID2].parameterSet
+    return_interset = g_apiDict[apiID1].returnSet & g_apiDict[apiID2].returnSet
+    return para_interset, return_interset
+
+
+def GetUnionset(apiID1, apiID2):
+    global g_apiDict
+    para_unionset = g_apiDict[apiID1].parameterSet | g_apiDict[apiID2].parameterSet
+    return_unionset = g_apiDict[apiID1].returnSet | g_apiDict[apiID2].returnSet
+    return para_unionset, return_unionset
+
+
+# compute the edge between two apis
+# if have common para/return type, then have an edge between the two operations/apis
+def GetEdge_half(interSet, unionSet):
+    edge_unwei = 0
+    edge_wei = 0
+    if len(unionSet) == 0:
+        return -1, -1
+    if len(interSet) != 0:
+        edge_unwei = 1
+        edge_wei = len(interSet) / float(len(unionSet))
+    # print edge_unwei, edge_wei
+    return edge_unwei, edge_wei
+
+# measure the meg-level 's interface cohesion'
+
+
+def Metric_msg_cohesion(clusterID):
+    global g_clusterID2Interf2APIDict
+    apiIDList = getAllAPIForCluster(clusterID)
+    if len(apiIDList) == 1:
+        cohesion_wei = 1
+    else:
+        from itertools import combinations
+        apiIDPairList = list(combinations(apiIDList, 2))
+        fenmu = len(apiIDPairList)  # perfect graph's edge number
+        apiSimList = list()
+        for apiPair in apiIDPairList:
+            [para_interset, return_interset] = GetIntersect(
+                apiPair[0], apiPair[1])
+            [para_unionset, return_unionset] = GetUnionset(
+                apiPair[0], apiPair[1])
+
+            [para_unweight, para_weight] = GetEdge_half(
+                para_interset, para_unionset)
+            [return_unweight, return_weight] = GetEdge_half(
+                return_interset, return_unionset)
+            # FEITO
+            if para_weight != -1 and return_weight != -1:
+                sim = (para_weight + return_weight) / float(2.0)
+                apiSimList.append(sim)
+
+            elif para_weight == -1 and return_weight != -1:
+                sim = return_weight
+                apiSimList.append(sim)
+            elif para_weight != -1 and return_weight == -1:
+                sim = para_weight
+                apiSimList.append(sim)
+        cohesion_wei = sum(apiSimList) / float(len(apiSimList))
+    return cohesion_wei
+
+# get api list for a cluster
+# g_clusterID2Interf2APIDict[clusterID][interface] = [api id ....]
+
+
+def getAllAPIForCluster(clusterID):
+    apiIDList = list()
+    global g_clusterID2Interf2APIDict
+    for interface in g_clusterID2Interf2APIDict[clusterID]:
+        apis = g_clusterID2Interf2APIDict[clusterID][interface]
+        apiIDList.extend(apis)
+    return apiIDList
+
+
+if __name__ == '__main__':
+    apiFileName = sys.argv[1]
+
+    global g_clusterID2Interf2APIDict  # [clusterID][interface] = [api id ....]
+    global g_apiDict
+
+    [g_clusterID2Interf2APIDict, g_apiDict] = ReadAPIFile(
+        apiFileName)
+    msg_cohesion_wei_list = list()
+    if len(g_clusterID2Interf2APIDict) == 0:
+        tmp = ['avg_msg_cohesion', str(1), 'interface_numb', str(
+            0), 'clusterHasinf', str(0)]
+        print(','.join(tmp))
+    else:
+        for clusterID in g_clusterID2Interf2APIDict:
+            msg_cohesion_wei = Metric_msg_cohesion(clusterID)
+            #print ('cluster,' + str(clusterID) + ',' + str(msg_cohesion_wei) )
+            msg_cohesion_wei_list.append(msg_cohesion_wei)
+        msg_avg_wei = sum(msg_cohesion_wei_list) / \
+            float(len(msg_cohesion_wei_list))
+
+        interface_number = 0
+        for clusterID in g_clusterID2Interf2APIDict:
+            interface_number += len(g_clusterID2Interf2APIDict[clusterID])
+        #print ('interface number=', interface_number)
+        tmp = ['avg_msg_cohesion', str(msg_avg_wei), 'interface_numb', str(
+            interface_number), 'clusterHasinf', str(len(g_clusterID2Interf2APIDict))]
+        print(','.join(tmp))
+        # print apidetail, using do and m_cohesion_wei_list interface_number_list
+        #print('\ninterface msg cohesion detail:')
+        # for index in range(0, len(msg_cohesion_wei_list)):
+        #    print(msg_cohesion_wei_list[index])
